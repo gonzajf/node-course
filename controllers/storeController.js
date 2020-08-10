67e44e1ab2e81c8da018c19f99383a4ac6acdf2b
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-10 11:29:33 -0300

    query db to get stores and show in new template.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,3 +14,10 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Succesfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+    
+    const stores = await Store.find();
+    console.log(stores);
+    res.render('stores', {title: 'Stores', stores});
+}
