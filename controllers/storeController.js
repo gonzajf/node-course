8f1cab597163d9de4726ff3b728b7456d9829974
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-10 12:14:24 -0300

    edit store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -18,6 +18,27 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
     
     const stores = await Store.find();
-    console.log(stores);
     res.render('stores', {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+
+    //find the store
+    const store = await Store.findOne({_id: req.params.id});
+    //confirm that you own the store to edit
+    // TODO 
+    //render the edit form
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+    
+}
+
+exports.updateStore = async (req, res) => {
+
+    //find and udpdate the store
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true,
+        runValidators: true        
+    }).exec();
+    req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/stores/${store._id}/edit`)
 }
