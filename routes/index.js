f43aff3df87f24e71f5c93f855c79844aad1b3e8
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-13 12:05:21 -0300

    register and log in user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -36,6 +37,7 @@ router.get('/register', userController.registerForm);
 //log user in
 router.post('/register', 
     userController.validateRegister,
-    catchErrors(userController.register));
+    catchErrors(userController.register),
+    authController.login);
 
 module.exports = router;
