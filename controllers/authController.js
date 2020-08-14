208856f326c2d9aa0f275370e99d3ee0dc23e405
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-13 22:31:16 -0300

    gravatar and isLoggedin method added

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,5 +11,15 @@ exports.login = passport.authenticate('local', {
 exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'You are now logged out!');
-    res.redirect('/')
+    res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+
+    if(req.isAuthenticated()) {
+        next();
+        return; 
+    }
+    req.flash('error', 'You must be logged in to do that');
+    res.redirect('login');
 }
