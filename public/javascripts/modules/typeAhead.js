90e06fd65b76eca53e8dcfea1879ef815e8bd464
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-15 22:12:34 -0300

    search form api

---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+function searchResultsHTML(stores) {
+    return stores.map(store => {
+        return `
+            <a href="/store/${store.slug}" class="search__result">
+                <strong>${store.name}</strong>
+            </a>
+        `;
+    }).join('');
+}
+
+function typeAhead(search) {
+    if(!search) {
+        return;
+    }
+    const searchInput = search.querySelector('input[name="search"]');
+    const searchResults = search.querySelector('.search__results');
+
+    searchInput.on('input', function() {
+        if(!this.value) {
+            searchResults.style.display = 'none';
+            return;
+        }
+        searchResults.style.display = 'block';
+        searchResults.innerHTML = '';
+
+        axios
+            .get(`/api/search?q=${this.value}`)
+            .then(res => {
+                if(res.data.length) {
+                    const html = searchResultsHTML(res.data);
+                    console.log(html);
+                    searchResults.innerHTML = html;
+                }
+            });
+    });
+};
+
+export default typeAhead;
