24913bcaf1da9c09eda27a45df6eb5b952318222
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-15 23:20:58 -0300

    adding selecting with keys after search

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -38,6 +38,32 @@ function typeAhead(search) {
                 console.error(err);
             });
     });
-};
+
+    //handle keyboard input
+    searchInput.on('keyup', (e) => {
+        if(![38, 40, 13].includes(e.keyCode)) {
+            return;
+        }
+        const activeClass = 'search__result--active';
+        const current = search.querySelector(`.${activeClass}`);
+        const items = search.querySelectorAll('.search__result');
+        let next;
+        if(e.keyCode === 40 && current) {
+            next = current.nextElementSibling || items[0];
+        } else if(e.keyCode === 40) {
+            next = items[0];
+        } else if(e.keyCode === 38 && current) {
+            next = current.previousElementSibling || items[items.length-1];
+        } else if(e.keyCode === 38) {
+            next = items[items.length-1];
+        } else if(e.keyCode === 13 && current.href) {
+            window.location = current.href;
+        }
+        if(current) {
+            current.classList.remove(activeClass);
+        }
+        next.classList.add(activeClass);
+    });
+}
 
 export default typeAhead;
