12138bf7a2faccad5dca1964aebe241e6d8f57c2
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-18 15:36:47 -0300

    adding animtation when a store is hearted

--- public/javascripts/modules/heart.js
+++ public/javascripts/modules/heart.js
@@ -9,6 +9,10 @@ function ajaxHeart(e) {
         .then(res => {
             const isHearted = this.heart.classList.toggle('heart__button--hearted');
             $('.heart-count').textContent = res.data.hearts.length;
+            if(isHearted) {
+                this.heart.classList.add('heart__button--float');
+                setTimeout(() => this.heart.classList.remove('heart__button--float'), 2500);
+            }
         })
         .catch(err => {
             consoler.error(err);
