22a8f1330247f3d998528abf84c997cad0ababbb
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-18 15:23:21 -0300

    automatically show heart list length on the screen without refreshing the page

---
+++ public/javascripts/modules/heart.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {$} from './bling';
+
+function ajaxHeart(e) {
+    e.preventDefault();
+    
+    axios
+        .post(this.action)
+        .then(res => {
+            const isHearted = this.heart.classList.toggle('heart__button--hearted');
+            $('.heart-count').textContent = res.data.hearts.length;
+        })
+        .catch(err => {
+            consoler.error(err);
+        })
+}
+
+export default ajaxHeart;
