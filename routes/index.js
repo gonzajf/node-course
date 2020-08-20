012c123e5905fd9a25bd28646dc0eb0905d0ae1a
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-20 14:54:00 -0300

    saving reviews to db

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
-const { Mongoose } = require('mongoose');
+
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
@@ -57,6 +58,10 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post('/reviews/:id', 
+    authController.isLoggedIn, 
+    catchErrors(reviewController.addReview));
+
 /**
  * API
  */
